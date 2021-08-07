07b4e3efc892803ce8baecaa166137a403c162d2
corne.hoskam <choskam@hotmail.com>
2021-08-07 11:25:28 +0200

    Cleaning warnings

--- Services/IRazorPublishEventService.cs
+++ Services/IRazorPublishEventService.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Threading.Tasks;
 using Umbraco9_Blazor.Models;
 
 namespace Umbraco9_Blazor.Services
--- Services/RazorPublishEventService.cs
+++ Services/RazorPublishEventService.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using Umbraco9_Blazor.Annotations;
 using Umbraco9_Blazor.Models;
 
